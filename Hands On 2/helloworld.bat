26cbc36a84575c1c01d3c9f3a0e01994fbc90b9e
savaDev25 <133390440+savaDev25@users.noreply.github.com>
2025-03-16 11:54:31 -0600

    Commit - Firts version of helloworld.bat has been completed

---
+++ Hands On 2/helloworld.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM Creando un Hello World sencillo
+echo "Hello World"
+pause
+
+REM Listando el contenido del directorio actual
+dir
+pause
+
+REM Creando un subdirectorio "Test"
+mkdir Test
+pause
+
+REM Cambiando al subdirectorio Test
+cd Test
+pause
+
+REM Listando el contenido del subdirectorio Test
+dir
+pause
